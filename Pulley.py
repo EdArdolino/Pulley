58255c87685d0abb256ac9649333d95b7949d143
Ed Ardolino <edwardardolino@gmail.com>
2022-06-29 15:26:50 -0400

    Updated Pulley.py

--- Pulley.py
+++ Pulley.py
@@ -1,2 +1,14 @@
 from requests_html import HTMLSession
 import time
+
+##
+# @Author: Ed Ardolino
+# @Version 1.0
+# @Creation Date: 6-29-2022
+##
+
+##
+#
+# Python webscraper to pull all GitHub repo chnages to your local machine 
+#
+##

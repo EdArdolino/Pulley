dc34497f03aa87d8c77bd72c4af21f1e44ec6e58
Ed Ardolino <edwardardolino@gmail.com>
2022-07-14 19:32:19 -0400

    Added starting function

--- Pulley.py
+++ Pulley.py
@@ -12,3 +12,11 @@ import time
 # Python webscraper to pull all GitHub repo chnages to your local machine 
 #
 ##
+
+# Function to start Pulley
+def start():
+    print("\n Choose which operation you would like to do:\n 1.) Pull existing repo's \n 2.) Add a repo")
+    response = input("Please enter a number from the list above: ")
+    while response not in {"1", "2"}:
+        response = input("Invalid input, please enter a number from the list above")
+
